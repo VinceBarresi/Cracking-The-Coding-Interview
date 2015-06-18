dc6a427c62d8e0587f142ef93aa2409d027ebb64
Vincent Barresi <vbarresi@amplify.com>
2015-06-18 15:57:43 -0400

    add C++ solution to 1.2

--- Chapter 1/1.2.cpp
+++ Chapter 1/1.2.cpp
@@ -5,6 +5,37 @@
   C or C++ which reverses a null-terminated string.
 */
 
-  void reverse(char* str) {
-    
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char* str = "null-terminated-string\0";
+
+void reverse(string str);
+
+int main(void)
+{
+  cout << "Reverse a null-terminated string";
+
+  reverse(str);
+
+  return 0;
+}
+
+void reverse(char* str) {
+  string newString = str;
+  int indeces = newString.length() - 1;
+  cout << indeces;
+  string stringArray[24]; 
+  string reverseString = ("");
+  
+  for (int i = 0; i < newString.length(); i++) {
+    stringArray[i] = newString.at(i);
   }
+  
+  for (int j = 0; j < newString.length(); j++) {
+    reverseString += stringArray[indeces - j];
+  }
+  cout << reverseString;
+}
