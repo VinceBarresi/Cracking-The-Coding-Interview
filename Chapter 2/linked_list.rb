780a183119ae140af6f966fb14f623e2467cb91a
Vincent Barresi <vbarresi@amplify.com>
2015-07-25 18:01:33 -0400

    update spacing in linked list class

--- Chapter 2/linked_list.rb
+++ Chapter 2/linked_list.rb
@@ -1,121 +1,118 @@
 class LinkedList
-
-    def initialize val
-        # Initialize a new node at the head
-        @head = Node.new(val, nil)
+  def initialize val
+    # init a new node at the head
+    @head = Node.new(val, nil)
     end
     
-    def add value
-        # Traverse the list and add a new node with passed in value
-        current = @head
-        while current.next_node != nil
-            current = current.next_node
-        end 
-        current.next_node = Node.new(value, nil)
-        self    
-    end
+  def add value
+    # traverse the list and add a new node with passed in value
+    current = @head
+    while current.next_node != nil
+      current = current.next_node
+    end 
+    current.next_node = Node.new(value, nil)
+    self    
+  end
 
-    def delete val
-        current = @head
-        if current.value == val
-            # If the head is the element to be delete, the head needs to be updated
-            @head = @head.next_node
-        else
-            # ... x -> y -> z
-            #   ... x->z
-            # need to link x -> z if y is deleted..
-            current = @head
-            while (current != nil) && (current.next_node != nil) && ((current.next_node).value != val)
-                current = current.next_node
-            end 
-
-            if (current != nil) && (current.next_node != nil)
-                current.next_node = (current.next_node).next_node
-            end
-        end
-    end
-    
-    def delete_rand_kth list
-        x = rand(10)
-        puts 'removing ' + x.to_s + 'kth element from LinkedList'
-        i = 1
-        current = @head
-        until i == x - 1 && current.next_node != nil 
-            current = current.next_node
-            i += 1
-        end
+  def delete val
+    current = @head
+    if current.value == val
+      # update head if its to be deleted
+      @head = @head.next_node
+    else
+    # ... x -> y -> z
+    #   ... x->z
+    # need to link x -> z if y is deleted..
+    current = @head
+    while (current != nil) && (current.next_node != nil) && ((current.next_node).value != val)
+      current = current.next_node
+      end 
+      if (current != nil) && (current.next_node != nil)
         current.next_node = (current.next_node).next_node
+      end
     end
+  end
+    
+  def delete_rand_kth list
+    x = rand(10)
+    puts 'removing ' + x.to_s + 'kth element from LinkedList'
+    i = 1
+    current = @head
+    until i == x - 1 && current.next_node != nil 
+      current = current.next_node
+      i += 1
+    end
+    current.next_node = (current.next_node).next_node
+  end
 
-    def delete_kth kth, list
-        x = kth
-        puts 'removing ' + x.to_s + 'kth element from LinkedList'
-        i = 1
-        current = @head
-        until i == x - 1 && current.next_node != nil 
-            current = current.next_node
-            i += 1
-        end
-        current.next_node = (current.next_node).next_node
+  def delete_kth kth, list
+    x = kth
+    puts 'removing ' + x.to_s + 'kth element from LinkedList'
+    i = 1
+    current = @head
+    until i == x - 1 && current.next_node != nil 
+      current = current.next_node
+      i += 1
+    end
+    current.next_node = (current.next_node).next_node
     end
     
-    def display
-        current = @head
-        full_list = [] 
-        while current.next_node != nil 
-            full_list += [current.value.to_s]
-            current = current.next_node
-        end
-        full_list += [current.value.to_s]
-        puts full_list.join('->')
+  def display
+    current = @head
+    full_list = [] 
+    while current.next_node != nil 
+      full_list += [current.value.to_s]
+      current = current.next_node
     end
+    full_list += [current.value.to_s]
+    puts full_list.join('->')
+  end
 
-    def list_to_int
-        current = @head
-        full_list = [] 
-        while current.next_node != nil 
-            full_list += [current.value.to_s]
-            current = current.next_node
-        end
-        full_list += [current.value.to_s]
-        full_list.join('').to_i
+  def list_to_int
+    current = @head
+    full_list = [] 
+    while current.next_node != nil 
+      full_list += [current.value.to_s]
+      current = current.next_node
     end
+    full_list += [current.value.to_s]
+    full_list.join('').to_i
+  end
 
-    def display_reverse_list
-        current = @head
-        full_list = []
-        while current.next_node != nil 
-            full_list += [current.value.to_s]
-            current = current.next_node
-        end
-        full_list += [current.value.to_s]
-        puts full_list.reverse.join('->')
+  def display_reverse_list
+    current = @head
+    full_list = []
+    while current.next_node != nil 
+      full_list += [current.value.to_s]
+      current = current.next_node
     end
+    full_list += [current.value.to_s]
+    puts full_list.reverse.join('->')
+  end
 
-    def check_if_palindrome
-        current = @head
-        full_list = []
-        while current.next_node != nil
-            full_list += [current.value.to_s]
-            current = current.next_node
-        end
-        full_list += [current.value.to_s]
-        if full_list == full_list.reverse
-            puts 'List is a Palindrome'
-        else
-            puts 'List is not a Palindrome'
-        end
+  def check_if_palindrome
+    current = @head
+    full_list = []
+    while current.next_node != nil
+      full_list += [current.value.to_s]
+      current = current.next_node
     end
-
-    def list_reverse_to_int
-        current = @head
-        full_list = [] 
-        while current.next_node != nil 
-            full_list += [current.value.to_s]
-            current = current.next_node
-        end
-        full_list += [current.value.to_s]
-        full_list.reverse.join('').to_i
+    full_list += [current.value.to_s]
+    if full_list == full_list.reverse
+      puts 'List is a Palindrome'
+    else
+      puts 'List is not a Palindrome'
     end
+  end
 
+  def list_reverse_to_int
+    current = @head
+    full_list = [] 
+    while current.next_node != nil 
+      full_list += [current.value.to_s]
+      current = current.next_node
+    end
+    full_list += [current.value.to_s]
+    full_list.reverse.join('').to_i
+  end
 end
