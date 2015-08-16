8d5cb1ef7aa7f5f083d9dec8f6e2941f85415497
Vincent Barresi <vbarresi@amplify.com>
2015-08-16 16:26:10 -0400

    update expect with true false predicate

--- spec/1.1_spec.rb
+++ spec/1.1_spec.rb
@@ -5,24 +5,24 @@ describe 'find unique with data structures' do
   it 'has unique elements' do
     str = 'abcdefghijklmnopqrstuvwxyz'
     str_array = str.split("")
-    expect(find_unique_with_data_structures?(str_array)).to eq(true)
+    expect(find_unique_with_data_structures?(str_array)).to be_truthy
   end
 
   it 'does not have unique elements' do
     str = 'aabbccdd'
     str_array = str.split("")
-    expect(find_unique_with_data_structures?(str_array)).to eq(false)
+    expect(find_unique_with_data_structures?(str_array)).to be_falsy
   end
 end
 
 describe 'find unique without data structures' do
    it 'has unique elements' do
     str = 'zyxwvutsrqponmlkjighfedcba'
-    expect(find_unique_without_data_structures?(str)).to eq(true)
+    expect(find_unique_without_data_structures?(str)).to be_truthy
   end
 
   it 'does not have unique elements' do
     str = 'ddccbbaa'
-    expect(find_unique_without_data_structures?(str)).to eq(false)
+    expect(find_unique_without_data_structures?(str)).to be_falsy
   end
 end
