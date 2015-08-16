ffa7c562a16b4cc609cd50dd49fc1d9c24438d45
Vincent Barresi <vbarresi@amplify.com>
2015-08-16 12:57:39 -0400

    update spec describe

--- spec/1.1_spec.rb
+++ spec/1.1_spec.rb
@@ -1,22 +1,24 @@
 require_relative 'spec_helper'
 require_relative '../chapter1/1.1'
 
-describe 'string' do
+describe 'find unique with data structures' do
   it 'has unique elements' do
     str = 'abcdefghijklmnopqrstuvwxyz'
     str_array = str.split("")
     expect(find_unique_with_data_structures?(str_array)).to eq(true)
   end
 
-  it 'has unique elements' do
-    str = 'zyxwvutsrqponmlkjighfedcba'
-    expect(find_unique_without_data_structures?(str)).to eq(true)
-  end
-
   it 'does not have unique elements' do
     str = 'aabbccdd'
     str_array = str.split("")
     expect(find_unique_with_data_structures?(str_array)).to eq(false)
+  end
+end
+
+describe 'find unique without data structures' do
+   it 'has unique elements' do
+    str = 'zyxwvutsrqponmlkjighfedcba'
+    expect(find_unique_without_data_structures?(str)).to eq(true)
   end
 
   it 'does not have unique elements' do
---
+++ spec/1.5_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'spec_helper'
+require_relative '../chapter1/1.5'
+
+describe 'compress string' do
+  it 'returns characters of a string with their count' do
+    str = 'aabbbccdeeee'
+    expect(compress_string(str).to eq('a2b3c2d1e4')
+  end
+end
