c1239f46de2e200dc82b93bc2f65ebbf1a477129
Vincent Barresi <vincebarresi@gmail.com>
2015-11-01 14:23:23 -0500

    fix tab spacing

--- chapter4/binary_search_tree.rb
+++ chapter4/binary_search_tree.rb
@@ -2,22 +2,20 @@ require_relative 'tree_node'
 
 # BST
 class BinarySearchTree
-	
-	# initialize root node
-	def initialize val
-		@root = TreeNode.new(val)
-	end
-
+  # initialize root node
+  def initialize val
+    @root = TreeNode.new(val)
+  end
   # values lesser than root will be inserted as a left child
   # values greater than root will be inserted as a right child
-	def push val
-		current_node = @root
-		if val < current_node.value && current_node.left == nil 
-			push_left_side(val)
-			current_node = current_node.left
-		elsif val > current_node.value && current_node.right == nil 
-			push_right_side(val)
-			current_node = current_node.right
-		end
-	end
+  def push val
+    current_node = @root
+    if val < current_node.value && current_node.left == nil 
+      push_left_side(val)
+      current_node = current_node.left
+      elsif val > current_node.value && current_node.right == nil 
+        push_right_side(val)
+	current_node = current_node.right
+      end
+    end
 end
