4365ab149200a05baec956f7b5185c463b524695
vincebarresi <vincebarresi@gmail.com>
2015-12-04 23:39:23 -0500

    refactor compress string

--- chapter1/1.5.rb
+++ chapter1/1.5.rb
@@ -24,14 +24,10 @@ def compress_string str
       last_char = arr_str[index]
       count = 1
     end
+  end
   index += 1
-  end
   compressed_str = new_str + last_char + count.to_s
-  if compressed_str.length < str.length
-    return compressed_str
-  else
-    return str
-  end
+  compressed_str.length < str.length ? compressed_str : str
 end
 
 p compress_string 'aabcccccaaa'
