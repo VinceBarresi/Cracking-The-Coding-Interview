dcbe78b8feb7723b5de18839f674bdc48bb22b13
vincebarresi <vincebarresi@gmail.com>
2015-12-06 20:18:16 -0500

    clean up method

--- chapter2/2.2.rb
+++ chapter2/2.2.rb
@@ -8,25 +8,7 @@
 require_relative 'list_node'
 require_relative 'linked_list'
 
-puts 'Creating LinkedList of 10 Node elements with random values'
-
-list = LinkedList.new(10)
-
-rand_arr = Array.new
-i = 0
-
-while i < 9
-  rand_arr[i] = rand(10)
-  i += 1
+def delete_random_kth
+	list = gen_random_list
+	delete_rand_kth list
 end
-
-rand_arr.each {|n| list.add(n) }
-
-puts "Displaying Linked List:"
-list.display
-
-list.delete_rand_kth list
-
-puts "\n"
-
-list.display
