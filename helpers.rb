467fea376938df35aca9781334fdbc3b46363a75
vincebarresi <vincebarresi@gmail.com>
2015-12-06 20:16:26 -0500

    add gen random array and linked list methods

--- helpers.rb
+++ helpers.rb
@@ -1,3 +1,6 @@
+require_relative 'chapter2/linked_list'
+require_relative 'chapter2/list_node'
+
 def depth array
   return 0 if array.class != Array
   result = 1
@@ -8,4 +11,21 @@ def depth array
     end
   end
   result
+end
+
+def gen_random_list list_size
+	list = LinkedList.new(list_size)
+	rand_arr = gen_random_array(list_size)
+	rand_arr.each { |n| list.add(n) }
+	list
+end
+
+def gen_random_array arr_size
+	rand_arr = Array.new
+	i = 0
+	while i < arr_size - 1
+  	rand_arr[i] = rand(arr_size)
+  	i += 1
+	end
+	rand_arr
 end
