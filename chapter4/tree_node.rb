331f4c43a6405aeb3b443abc2cd4116661adab17
Vincent Barresi <vincebarresi@gmail.com>
2016-04-13 14:05:10 -0400

    Update tree_node.rb

--- chapter4/tree_node.rb
+++ chapter4/tree_node.rb
@@ -1,13 +1,13 @@
 require_relative 'empty_node'
 
 class TreeNode
-	attr_reader :value
-	attr_accessor :left, :right
-	
-	# initialize tree node with a value and two empty sides
-	def initialize val
-		@value = val
-		@left = EmptyNode.new
-		@right = EmptyNode.new
-	end
+  attr_reader :value
+  attr_accessor :left, :right
+  
+  # initialize tree node with a value and two empty sides
+  def initialize val
+    @value = val
+    @left = EmptyNode.new
+    @right = EmptyNode.new
+  end
 end
